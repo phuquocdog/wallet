db892a07aac0ddd19fbd88f4034af37905ed3799
Thien Tran <fcduythien@gmail.com>
2021-09-29 23:14:07 +0700

    Optimize code

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['transform-inline-environment-variables'],
+  plugins: [],
   env: {
     production: {
       plugins: ['transform-remove-console']
--- blue_modules/Privacy.ios.js
+++ blue_modules/Privacy.ios.js
@@ -1,10 +1,7 @@
-import { enabled } from 'react-native-privacy-snapshot';
 export default class Privacy {
   static enableBlur() {
-    enabled(true);
   }
 
   static disableBlur() {
-    enabled(false);
   }
 }
